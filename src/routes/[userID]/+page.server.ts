3161a6c4e01faa9bfb61be456d215b9e2348f43f
Theo Bertilsson <131893433+TheoBertilsson@users.noreply.github.com>
2024-09-07 15:33:51 +0200

    Add: page.server

---
+++ src/routes/[userID]/+page.server.ts
@@ -0,0 +1,7 @@
+import type { PageServerLoad } from '../../../.svelte-kit/types/src/routes/[userID]/$types';
+
+export const load = (async ({params}) => {
+	const userID = params.userID
+	
+	return {};
+}) satisfies PageServerLoad;
