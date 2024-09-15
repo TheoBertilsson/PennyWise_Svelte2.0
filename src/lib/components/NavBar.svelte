f8c232440414c7e7d4d9fcfc28e3c1c6eb01e487
Theo Bertilsson <131893433+TheoBertilsson@users.noreply.github.com>
2024-09-15 21:43:04 +0200

    Add: Overview page UI

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,13 +1,42 @@
-<nav class="fixed top-0 left-0 h-full w-64 bg-secondary text-primary flex flex-col justify-between p-4 items-center">
-  <div class="w-full flex flex-col justify-center items-center">
-    <h2 class="text-2xl font-bold mb-10">PennyWise</h2>
-    <ul class="">
-      <li class="mb-2"><a href="#" class="text-lg hover:text-gray-400">Overview</a></li>
-      <li class="mb-2"><a href="#" class="text-lg hover:text-gray-400">Transactions</a></li>
-      <li class="mb-2"><a href="#" class="text-lg hover:text-gray-400">Budget</a></li>
-      <li class="mb-2"><a href="#" class="text-lg hover:text-gray-400">Pot</a></li>
-      <li class="mb-2"><a href="#" class="text-lg hover:text-gray-400">Recurring</a></li>
-    </ul>
-  </div>
-  <button class="text-lg hover:text-gray-400">Sign Out</button>
-</nav>
+<script>
+	let isNavOpen = true;
+
+	function toggleNav() {
+		isNavOpen = !isNavOpen;
+	}
+</script>
+
+{#if isNavOpen}
+	<nav
+		class=" top-0 left-0 h-screen w-64 bg-secondary text-primary flex flex-col justify-between items-center"
+	>
+		<div class="w-full flex flex-col justify-center items-center relative">
+			<button class="absolute top-0 left-0 text-lg w-12 h-12" on:click={toggleNav}>
+				<i class="bi bi-x"></i>
+			</button>
+			<h2 class="text-2xl font-bold my-10 p-2">PennyWise</h2>
+
+			<ul class="p-2">
+				<li class="mb-2"><a href="/overview" class="text-lg hover:text-gray-400">Overview</a></li>
+				<li class="mb-2">
+					<a href="/transaction" class="text-lg hover:text-gray-400">Transactions</a>
+				</li>
+				<li class="mb-2"><a href="/budget" class="text-lg hover:text-gray-400">Budget</a></li>
+				<li class="mb-2"><a href="/Pot" class="text-lg hover:text-gray-400">Pot</a></li>
+				<li class="mb-2"><a href="/Recurring" class="text-lg hover:text-gray-400">Recurring</a></li>
+			</ul>
+		</div>
+		<button class="text-lg hover:text-gray-400 p-4">Sign Out</button>
+	</nav>
+{:else}
+	<button class="top-0 left-0 m-6 h-full text-xl" on:click={toggleNav}>
+		<i class="bi bi-list"></i>
+	</button>
+{/if}
+
+<style>
+	.bi-x,
+	.bi-list {
+		font-size: 1.5rem;
+	}
+</style>
--- src/routes/Overview/+page.svelte
+++ src/routes/Overview/+page.svelte
@@ -1,12 +1,45 @@
 <script lang="ts">
-  import AuthCheck from '$lib/components/AuthCheck.svelte';
-  import NavBar from '$lib/components/NavBar.svelte';
+	import AuthCheck from '$lib/components/AuthCheck.svelte';
+	import NavBar from '$lib/components/NavBar.svelte';
 </script>
+
 <AuthCheck>
-<header>
-  <NavBar />
-</header>
-<main>
-  <h1>Overview</h1>
-</main>
+	<main class="flex">
+		<NavBar />
+		<div class="w-full p-8">
+			<h1 class="text-3xl font-bold mb-12">Current month</h1>
+			<div class="flex justify-between space-x-4">
+				<div class="w-1/3 bg-gray-200 p-4 rounded-lg shadow-md flex flex-col">
+					<span class="font-semibold">Remaining</span>
+					<span class="text-2xl">0.0$</span>
+				</div>
+				<div class="w-1/3 bg-gray-200 p-4 rounded-lg shadow-md flex flex-col">
+					<span class="font-semibold">Income</span>
+					<span class="text-2xl">0.0$</span>
+				</div>
+				<div class="w-1/3 bg-gray-200 p-4 rounded-lg shadow-md flex flex-col">
+					<span class="font-semibold">Expenses</span>
+					<span class="text-2xl">0.0$</span>
+				</div>
+			</div>
+			<div class="grid grid-cols-2 gap-4 mt-16">
+				<a href="/savings" class="relative bg-gray-200 p-4 rounded-lg shadow-md h-32 ">
+					<button class="absolute top-2 right-2 text-xl">+</button>
+					<span class="text-xl font-semibold">Savings</span>
+				</a>
+				<a href="/budget" class="relative bg-gray-200 p-4 rounded-lg shadow-md h-90 row-span-2">
+					<button class="absolute top-2 right-2 text-xl">+</button>
+					<span class="text-xl font-semibold">Budget</span>
+				</a>
+				<a href="/transaction" class="relative bg-gray-200 p-4 rounded-lg shadow-md h-96 row-span-2">
+					<button class="absolute top-2 right-2 text-xl">+</button>
+					<span class="text-xl font-semibold">Transaction</span>
+				</a>
+				<a href="/recurring" class="relative bg-gray-200 p-4 rounded-lg shadow-md h-44">
+					<button class="absolute top-2 right-2 text-xl">+</button>
+					<span class="text-xl font-semibold">Recurring</span>
+				</a>
+			</div>
+		</div>
+	</main>
 </AuthCheck>
