d574fdf8ad3f868fab1cbebac823b943d73d1d50
Theo Bertilsson <131893433+TheoBertilsson@users.noreply.github.com>
2024-10-22 16:08:10 +0200

    Update TransactionBox.svelte

--- src/lib/components/overviewComponents/TransactionBox.svelte
+++ src/lib/components/overviewComponents/TransactionBox.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
-	export let transactions;
+	import type { Transaction } from '../models/types';
+
+	export let transactions: Transaction[];
+	const transactionsWithDate = transactions.map((transaction) => {
+		const createdAt = new Date(transaction.createdAt);
+		const month = createdAt.toLocaleString('default', { month: 'short' });
+		const day = createdAt.getDate();
+		const year = createdAt.getFullYear();
+		return {
+			...transaction,
+			createdAt: `${day} ${month} ${year}`,
+		};
+	});
 </script>
 
-<div class="border-b-2 border-zinc-500">
+<div class="mb-4 border-b-2 border-zinc-500">
 	<span class="text-xl font-semibold">Transactions</span>
 </div>
-<div class="scrollbar-hide flex max-h-[90%] flex-col gap-2 overflow-scroll p-3">
-	{#each transactions as transaction}
-		<div class="flex justify-between rounded-lg border-2 border-zinc-400 p-2 shadow-md">
-			<span>{transaction.name}</span>
-			<span class="capitalize text-zinc-500">{transaction.category}</span>
-			<span>{transaction.price} $</span>
+<div class="scrollbar-hide flex max-h-[90%] flex-col gap-4 overflow-scroll">
+	{#each transactionsWithDate as transaction}
+		<div class="flex justify-between border-b-2 border-[#f7f7f7] p-2">
+			<div class="flex items-center gap-4">
+				<span class="h-8 w-8 rounded-full bg-text"></span>
+				<span class="line-clamp-1 capitalize">{transaction.name}</span>
+			</div>
+			<div class="flex flex-col items-end">
+				<span class="font-bold text-base">{transaction.price}$</span>
+				<span class="capitalize text-sm text-text">{transaction.createdAt}</span>
+			</div>
 		</div>
 	{/each}
 </div>
