7ba3cf5df3e253e324328499efc52a1717c953fa
Theo Bertilsson <131893433+TheoBertilsson@users.noreply.github.com>
2024-11-21 08:58:37 +0100

    Add: trash button

--- src/lib/components/budgetItemsList.svelte
+++ src/lib/components/budgetItemsList.svelte
@@ -12,15 +12,19 @@
 {#if !clickedChartInfo.budgetCategory}
 	<div class="flex w-full flex-col items-center justify-center gap-2 px-5">
 		<div class="flex w-full justify-between">
-			<span class=" text-lg font-bold underline">All budget items</span>
-			<span class=" text-lg font-bold underline">{totalSum} kr</span>
+			<span class=" text-lg font-bold underline">All</span>
+			<span class=" text-lg font-bold underline">{totalSum.toLocaleString()} kr</span>
 		</div>
 
 		{#each budgetItems as item}
 			<div class="flex w-full justify-between border-b-2 border-[#0000070]">
 				<span class="font-bold">{item.subCategory}</span>
 				<span class="capitalize opacity-70">{item.category}</span>
-				<h3>{item.price}kr</h3>
+				<h3>{item.price.toLocaleString()}kr</h3>
+				<button
+					class="flex h-6 w-6 items-center justify-center rounded-sm border-[0.5px] border-red-500 bg-white p-1 hover:bg-slate-100 active:bg-slate-300"
+					><img src="/trash.svg" alt="Trashcan" /></button
+				>
 			</div>
 		{/each}
 	</div>
@@ -29,7 +33,7 @@
 		{#if clickedChartInfo.budgetCategory && clickedChartInfo.budgetSum}
 			<div class="flex w-full justify-between">
 				<span class=" text-lg font-bold capitalize underline"
-					>{clickedChartInfo.budgetCategory} budget items</span
+					>{clickedChartInfo.budgetCategory}</span
 				>
 				<span class=" text-lg font-bold underline"
 					>{clickedChartInfo.budgetSum.toLocaleString()} kr</span
