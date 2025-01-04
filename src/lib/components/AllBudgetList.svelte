49c1efac4f940bb5ed8056c30141e7914be0d2cf
Theo Bertilsson <131893433+TheoBertilsson@users.noreply.github.com>
2025-01-04 21:04:03 +0100

    Fix: Budget List

--- src/lib/components/AllBudgetList.svelte
+++ src/lib/components/AllBudgetList.svelte
@@ -24,7 +24,7 @@
 		.reduce((sum, item) => (sum += item.price), 0);
 </script>
 
-<div class="flex w-full items-center justify-between">
+<div class="flex w-full items-center justify-between px-4">
 	<div class="flex flex-col items-center justify-center">
 		<span class="text-lg font-bold underline">Income:</span>
 		<span>{totalIncome.toLocaleString()} kr</span>
@@ -42,15 +42,16 @@
 		class="flex w-full justify-between border-b-2 border-[#0000070]"
 	>
 		<input type="hidden" name="id" value={item.id} />
-		<span class="font-bold">{item.subCategory}</span>
-		<span class="capitalize opacity-70">{item.category}</span>
-		<h3>{item.price.toLocaleString()}kr</h3>
+		<span class="font-semibold">{item.subCategory}</span>
 
-		<button
-			type="submit"
-			class="flex h-6 w-6 items-center justify-center rounded-sm border-[0.5px] border-red-500 bg-white p-1 hover:bg-slate-100 active:bg-slate-300"
-		>
-			<img src="/trash.svg" alt="Trashcan" />
-		</button>
+		<div class="flex gap-2">
+			<span>{item.price.toLocaleString()} kr</span>
+			<button
+				type="submit"
+				class="flex h-6 w-6 items-center justify-center rounded-sm border-[0.5px] border-red-500 bg-white p-1 hover:bg-slate-100 active:bg-slate-300"
+			>
+				<img src="/trash.svg" alt="Trashcan" />
+			</button>
+		</div>
 	</form>
 {/each}
